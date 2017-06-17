403a686c037f16d15262a1c4c04242f7961c9e4a
Yang Luo <hsluoyz@qq.com>
2017-06-17 13:49:38 +0800

    Add comments to the test.

--- global_test.go
+++ global_test.go
@@ -32,6 +32,7 @@ func testGlobalEnforce(t *testing.T, tenant string, sub string, obj string, act 
 }
 
 func TestGlobalAdmin(t *testing.T) {
+	// admin can access anything.
 	testGlobalEnforce(t, "admin", "admin", "/", "GET", "nova", true)
 	testGlobalEnforce(t, "admin", "admin", "/admin/servers/detail", "GET", "nova", true)
 	testGlobalEnforce(t, "admin", "admin", "/admin/extensions", "GET", "nova", true)
@@ -39,36 +40,46 @@ func TestGlobalAdmin(t *testing.T) {
 	testGlobalEnforce(t, "admin", "admin", "/admin/flavors/detail", "GET", "nova", true)
 	testGlobalEnforce(t, "admin", "admin", "/admin/extensions", "GET", "nova", true)
 
+	// tenant1 cannot access admin's VM.
 	testGlobalEnforce(t, "tenant1", "user11", "/admin/servers/detail", "GET", "nova", false)
 	testGlobalEnforce(t, "tenant1", "user12", "/admin/servers/detail", "GET", "nova", false)
 	testGlobalEnforce(t, "tenant1", "user13", "/admin/servers/detail", "GET", "nova", false)
 
+	// tenant2 cannot access admin's VM.
 	testGlobalEnforce(t, "tenant2", "user2", "/admin/servers/detail", "GET", "nova", false)
 
+	// tenant3 cannot access admin's VM.
 	testGlobalEnforce(t, "tenant3", "user3", "/admin/servers/detail", "GET", "nova", false)
 }
 
 func TestGlobalEnable(t *testing.T) {
+	// user11 is the tenant administrator.
+	// user11 can access policy of his own tenant.
 	testGlobalEnforce(t,"tenant1", "user11", "/tenant1/metadata", "GET", "patron", true)
 	testGlobalEnforce(t, "tenant1", "user11", "/tenant1/metadata", "POST", "patron", true)
 	testGlobalEnforce(t, "tenant1", "user11", "/tenant1/policy", "GET", "patron", true)
 	testGlobalEnforce(t, "tenant1", "user11", "/tenant1/policy", "POST", "patron", true)
 
+	// user12 cannot access policy of his own tenant.
 	testGlobalEnforce(t,"tenant1", "user12", "/tenant1/metadata", "GET", "patron", false)
 	testGlobalEnforce(t, "tenant1", "user12", "/tenant1/metadata", "POST", "patron", false)
 	testGlobalEnforce(t, "tenant1", "user12", "/tenant1/policy", "GET", "patron", false)
 	testGlobalEnforce(t, "tenant1", "user12", "/tenant1/policy", "POST", "patron", false)
 
+	// tenant2 cannot access tenant1's policy.
 	testGlobalEnforce(t,"tenant2", "user2", "/tenant1/metadata", "GET", "patron", false)
 	testGlobalEnforce(t, "tenant2", "user2", "/tenant1/metadata", "POST", "patron", false)
 	testGlobalEnforce(t, "tenant2", "user2", "/tenant1/policy", "GET", "patron", false)
 	testGlobalEnforce(t, "tenant2", "user2", "/tenant1/policy", "POST", "patron", false)
 
+	// user2 is the tenant administrator.
+	// user2 can access policy of his own tenant.
 	testGlobalEnforce(t,"tenant2", "user2", "/tenant2/metadata", "GET", "patron", true)
 	testGlobalEnforce(t, "tenant2", "user2", "/tenant2/metadata", "POST", "patron", true)
 	testGlobalEnforce(t, "tenant2", "user2", "/tenant2/policy", "GET", "patron", true)
 	testGlobalEnforce(t, "tenant2", "user2", "/tenant2/policy", "POST", "patron", true)
 
+	// tenant3 cannot access anything.
 	testGlobalEnforce(t,"tenant3", "user3", "/tenant3/metadata", "GET", "patron", false)
 	testGlobalEnforce(t, "tenant3", "user3", "/tenant3/metadata", "POST", "patron", false)
 	testGlobalEnforce(t, "tenant3", "user3", "/tenant3/policy", "GET", "patron", false)
@@ -76,32 +87,34 @@ func TestGlobalEnable(t *testing.T) {
 }
 
 func TestGlobalRestrict(t *testing.T) {
-	testGlobalEnforce(t,"tenant1", "user11", "/tenant1/servers/detail", "GET", "nova", true)
-	testGlobalEnforce(t,"tenant1", "user12", "/tenant1/servers/detail", "GET", "nova", true)
-	testGlobalEnforce(t,"tenant1", "user13", "/tenant1/servers/detail", "GET", "nova", true)
-
+	// tenant2 and tenant3 cannot access tenant1's VM.
 	testGlobalEnforce(t,"tenant2", "user2", "/tenant1/servers/detail", "GET", "nova", false)
 	testGlobalEnforce(t,"tenant3", "user3", "/tenant1/servers/detail", "GET", "nova", false)
 
+	// tenant2 and tenant3 cannot access tenant1's disk.
 	testGlobalEnforce(t,"tenant2", "user2", "/tenant1/volumes/detail", "GET", "cinder", false)
 	testGlobalEnforce(t,"tenant3", "user3", "/tenant1/volumes/detail", "GET", "cinder", false)
 
+	// tenant2 and tenant3 cannot access admin's VM.
 	testGlobalEnforce(t, "tenant1", "user11", "/admin/servers/detail", "GET", "nova", false)
 	testGlobalEnforce(t, "tenant2", "user2", "/admin/servers/detail", "GET", "nova", false)
 	testGlobalEnforce(t, "tenant3", "user3", "/admin/servers/detail", "GET", "nova", false)
 }
 
 func TestGlobalTenant1(t *testing.T) {
+	// user11 can access anything in tenant1.
 	testGlobalEnforce(t,"tenant1", "user11", "/tenant1/servers/detail", "GET", "nova", true)
 	testGlobalEnforce(t,"tenant1", "user11", "/v2/images", "GET", "glance", true)
 	testGlobalEnforce(t,"tenant1", "user11", "/networks.json", "GET", "neutron", true)
 	testGlobalEnforce(t,"tenant1", "user11", "/tenant1/volumes/detail", "GET", "cinder", true)
 
+	// user12 can only access Nova.
 	testGlobalEnforce(t,"tenant1", "user12", "/tenant1/servers/detail", "GET", "nova", true)
 	testGlobalEnforce(t,"tenant1", "user12", "/v2/images", "GET", "glance", false)
 	testGlobalEnforce(t,"tenant1", "user12", "/networks.json", "GET", "neutron", false)
 	testGlobalEnforce(t,"tenant1", "user12", "/tenant1/volumes/detail", "GET", "cinder", false)
 
+	// user13 can only access Glance.
 	testGlobalEnforce(t,"tenant1", "user13", "/tenant1/servers/detail", "GET", "nova", false)
 	testGlobalEnforce(t,"tenant1", "user13", "/v2/images", "GET", "glance", true)
 	testGlobalEnforce(t,"tenant1", "user13", "/networks.json", "GET", "neutron", false)
